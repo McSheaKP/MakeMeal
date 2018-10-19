3921797fe6274752c76d1bba306b1af6785eb9ba
Kevin McShea <kevinmcshea@gmail.com>
2018-10-19 15:09:18 -0700

    added page nagivation and recipe-item to recipe-list communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MakeMeal';
+	title = 'MakeMeal';
+	loadedFeature = "recipe";
+	
+	onNavigate(feature: string){
+		this.loadedFeature = feature;
+
+	}
+
 }
