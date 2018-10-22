1956a6c6188a9eb729319754f92a831873d99cca
Kevin McShea <kevinmcshea@gmail.com>
2018-10-22 11:06:58 -0700

    added display of recipe components in recipe-detail.component

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model'
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 
 export class RecipeService{
@@ -7,7 +8,15 @@ export class RecipeService{
 	recipeSelected = new EventEmitter<Recipe>();
 	
 	private recipes: Recipe[] = [
-		new Recipe("A test recipe", "this is a test", 'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg')
+		new Recipe(
+			"A test recipe", 
+			"this is a test", 
+			'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg',
+			[
+				new Ingredient('Meat', 1),
+				new Ingredient('French Fries', 20),
+			]
+			)
 	];
 
 	getRecipes(){
