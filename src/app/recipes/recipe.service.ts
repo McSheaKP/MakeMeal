3b3dbe2514dba66b41abb3f792713ed245c16267
Kevin McShea <kevinmcshea@gmail.com>
2018-10-22 10:20:57 -0700

    migrated recipe parent/child communication to service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
 import { Recipe } from './recipe.model'
+import { EventEmitter } from '@angular/core';
+
 
 export class RecipeService{
+
+	recipeSelected = new EventEmitter<Recipe>();
 	
 	private recipes: Recipe[] = [
 		new Recipe("A test recipe", "this is a test", 'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg')
