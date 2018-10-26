e2cc39cc65373498105fd61af801c15a107be77b
Kevin McShea <kevinmcshea@gmail.com>
2018-10-25 21:27:27 -0700

    added basic forms function to shopping-edit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,11 +1,11 @@
 import { 
 	Component, 
 	OnInit, 
-	ElementRef, 
-	ViewChild, 
+
 } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -13,8 +13,7 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-	@ViewChild('nameInput') nameInputRef: ElementRef;
-	@ViewChild('amountInput') amountInputRef: ElementRef;
+
 
 
   constructor(private slService: ShoppingListService) { }
@@ -22,10 +21,9 @@ export class ShoppingEditComponent implements OnInit {
   ngOnInit() {
 	}
 	
-	onAddItem() {
-		const ingName = this.nameInputRef.nativeElement.value;
-		const ingAmount = this.amountInputRef.nativeElement.value;
-		const newIngredient = new Ingredient(ingName, ingAmount);
+	onAddItem(form: NgForm) {
+		const value = form.value;
+		const newIngredient = new Ingredient(value.name, value.amount);
 		this.slService.addIngredient(newIngredient);
 	}
 
